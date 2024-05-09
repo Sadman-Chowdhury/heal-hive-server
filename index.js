79cb93a932bd74547eecce0dabbac3c53d15fa44
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-05-09 19:27:46 +0600

    used .env to hide mongodb credentials

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-//master_doctor
-//L7fRplHHYzWRWvLI
 
 
 
-const uri = "mongodb+srv://master_doctor:L7fRplHHYzWRWvLI@cluster0.scfrsgh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.scfrsgh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
