7f45fa06635e1a735ccee5a6010400e0033ec519
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-05-10 21:11:09 +0600

    Get feature with specific id added for getting single services through id

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
@@ -34,6 +34,13 @@ async function run() {
     app.get('/services', async(req,res)=>{
         const cursor = serviceCollection.find()
         const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get('/services/:id', async(req,res)=>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await serviceCollection.findOne(query)
         res.send(result)
     })
 
