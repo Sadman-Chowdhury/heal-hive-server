e9b02f5f956ee30eb548942c473da3293bf44a97
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-05-12 00:00:08 +0600

    delete feature added for deleting a service from db

--- index.js
+++ index.js
@@ -80,6 +80,14 @@ async function run() {
 
     })
 
+    app.delete('/services/:id', async(req,res)=>{
+        const id = req.params.id
+        console.log('Delete from db', id)
+        const query = {_id: new ObjectId(id)}
+        const result = await serviceCollection.deleteOne(query)
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
