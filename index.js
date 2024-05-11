2957d899c602e549269f67b3b3183e4bb7b3057e
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-05-11 19:54:59 +0600

    Post feature added for posting bookings in DB

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
         res.send(result)
     })
 
+    app.post('/bookings', async (req, res)=>{
+        const newBooking = req.body
+        console.log(newBooking)
+        const result = await bookingCollection.insertOne(newBooking);
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
